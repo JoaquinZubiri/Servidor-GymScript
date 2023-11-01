d52f8392fb337b4065c4ca156a7b5533d4839248
Joaquin Zubiri <113479820+JoaquinZubiri@users.noreply.github.com>
2023-11-01 13:21:49 +0000

    Conexion BD nube

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("gimnasio", "root", "root", {
-  host: "localhost",
-  dialect: "mysql",
+// const db = new Sequelize("gimnasio", "root", "root", {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+const db = new Sequelize("b84l754fxnnbzxvu6l1f","ushawjsnhhwqvgjr","b8Ha7fIJK0HYPk9A0j7C",{
+    host: "b84l754fxnnbzxvu6l1f-mysql.services.clever-cloud.com",
+    port: "3306",
+    dialect: "mysql",
 });
 
 export default db;
