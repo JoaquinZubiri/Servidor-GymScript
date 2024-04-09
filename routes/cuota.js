0a3ff3c4eb110353c4a3302f0cd6d4ca58a69bd3
Joaquin Zubiri-PC <joaquinzubiri00@gmail.com>
2024-04-09 16:28:10 -0300

    fix: couta route

--- routes/cuota.js
+++ routes/cuota.js
@@ -1,6 +1,6 @@
-import { Router } from 'express';
-import { cuotaController } from '../controllers/cuotas.js';
-import { authAdmin, authUser } from '../middleware/auth.js';
+import { Router } from "express";
+import { cuotaController } from "../controllers/cuotas.js";
+import { authAdmin, authUser } from "../middleware/auth.js";
 
 export const cuotaRouter = Router();
 
@@ -9,17 +9,17 @@ export const cuotaRouter = Router();
 // url/cuotas?idInscripcion=1&&ultima=true --> Devuelve la ultima cuota de la inscripcion 1
 // url/cuotas?ultimas=true --> Devuelve las ultimas 2 cuotas de cada inscripcion
 // url/cuotas --> Devuelve todas las cuotas
-cuotaRouter.get('/', authAdmin, cuotaController.getAll);
+cuotaRouter.get("/", authUser, cuotaController.getAll);
 
-cuotaRouter.get('/:id', authAdmin, cuotaController.getById);
+cuotaRouter.get("/:id", authAdmin, cuotaController.getById);
 
 //Devuelve el estado de la cuota (true si esta vencida, false si no)
-cuotaRouter.get('/vencimiento/:id', authUser, cuotaController.getStateCuota);
+cuotaRouter.get("/vencimiento/:id", authUser, cuotaController.getStateCuota);
 
-cuotaRouter.patch('/:id', authAdmin, cuotaController.update);
+cuotaRouter.patch("/:id", authAdmin, cuotaController.update);
 
-cuotaRouter.post('/', authUser, cuotaController.pagar);
+cuotaRouter.post("/", authUser, cuotaController.pagar);
 
-cuotaRouter.post('/crear', authAdmin, cuotaController.create);
+cuotaRouter.post("/crear", authAdmin, cuotaController.create);
 
-cuotaRouter.delete('/:id', authAdmin, cuotaController.delete);
+cuotaRouter.delete("/:id", authAdmin, cuotaController.delete);
